d83957a29cd150637b515fa6a7783dedea3b456d
SassDavid <elethulyelenseg@hotmail.com>
2019-10-02 06:12:54 +0200

    docs, refactor (DefenderASR) Get-DefenderAsrRule
    top level try catch

--- WindowsDefender/DefenderASR/Lib/function-Get-DefenderAsrRule.ps1
+++ WindowsDefender/DefenderASR/Lib/function-Get-DefenderAsrRule.ps1
@@ -7,8 +7,11 @@ function Get-DefenderAsrRule {
         .OUTPUTS
             [PSCustomObject[]]
         .EXAMPLE
-            PS > Get-DefenderAsrRules
-        .EXAMPLE
+            Get-DefenderAsrRules
+
+            Will give you back the current configuration
+        .Notes
+            Will give back a PSCustomObject will nice human readable rule names and IDs
         .LINK
             https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-defender-exploit-guard/attack-surface-reduction-exploit-guard
     #>
@@ -23,27 +26,32 @@ function Get-DefenderAsrRule {
     }
 
     process {
-        $i = 0
-        Write-Verbose "We've $($pref.AttackSurfaceReductionRules_Ids.Count) configured"
-        if ($pref.AttackSurfaceReductionRules_Ids.Count -gt 0) {
-            $pref.AttackSurfaceReductionRules_Ids | ForEach-Object {
-                Write-Verbose "ASRGuid: $_"
-                $g = $_
-                $rules[$($g.ToLower())] | ForEach-Object {
-                    Write-Verbose "ASRName: $($_)"
-                    $o = $_.PSObject.Copy()
-                    Write-Verbose $_.Name
-                    Add-Member -InputObject $o -Name "Action" -MemberType NoteProperty -Value $($states["$($pref.AttackSurfaceReductionRules_Actions[$i])"].Name)
-                    Write-Verbose -Message "`$ActionID = $($pref.AttackSurfaceReductionRules_Actions[$i])"
-                    Add-Member -InputObject $o -Name "ActionId" -MemberType NoteProperty -Value $pref.AttackSurfaceReductionRules_Actions[$i]
-                    $o
-                }
-                $i++
-                # TODO: keep in mind that extending the returned properties, you have to extend the Select-Object as well
-            } | Sort-Object $Id | Select-Object Name, Action, ActionId, Guid
+        try {
+            $i = 0
+            Write-Verbose "We've $($pref.AttackSurfaceReductionRules_Ids.Count) configured"
+            if ($pref.AttackSurfaceReductionRules_Ids.Count -gt 0) {
+                $pref.AttackSurfaceReductionRules_Ids | ForEach-Object {
+                    Write-Verbose "ASRGuid: $_"
+                    $g = $_
+                    $rules[$($g.ToLower())] | ForEach-Object {
+                        Write-Verbose "ASRName: $($_)"
+                        $o = $_.PSObject.Copy()
+                        Write-Verbose $_.Name
+                        Add-Member -InputObject $o -Name "Action" -MemberType NoteProperty -Value $($states["$($pref.AttackSurfaceReductionRules_Actions[$i])"].Name)
+                        Write-Verbose -Message "`$ActionID = $($pref.AttackSurfaceReductionRules_Actions[$i])"
+                        Add-Member -InputObject $o -Name "ActionId" -MemberType NoteProperty -Value $pref.AttackSurfaceReductionRules_Actions[$i]
+                        $o
+                    }
+                    $i++
+                    # TODO: keep in mind that extending the returned properties, you have to extend the Select-Object as well
+                } | Sort-Object $Id | Select-Object Name, Action, ActionId, Guid
+            }
+            else {
+                Write-Warning "You don't have any ASR rules defined, we recommend to configure some!"
+            }
         }
-        else {
-            Write-Warning "You don't have any ASR rules defined, we recommend to configure some!"
+        catch {
+            Write-Host "Something went terribly wrong: $_" -ForegroundColor Yellow
         }
     }
 
