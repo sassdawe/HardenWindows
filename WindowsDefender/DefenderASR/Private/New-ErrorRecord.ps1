e9b06bfac9e365a6498602327d3ce0e1645df0d8
Sass David <skills@kolis.hu>
2020-07-21 20:37:18 +0200

    feat(DefenderASR): New-ErrorRecord
    needs more work

--- WindowsDefender/DefenderASR/Private/New-ErrorRecord.ps1
+++ WindowsDefender/DefenderASR/Private/New-ErrorRecord.ps1
@@ -7,29 +7,32 @@ function New-ErrorRecord {
         .OUTPUTS
 
         .EXAMPLE
-            throw ( New-ShouldErrorRecord -Message $testResult.FailureMessage -File $file -Line $lineNumber -LineText $lineText )
+            throw ( New-ErrorRecord -Message $testResult.FailureMessage -File $file -Line $lineNumber -LineText $lineText )
         .EXAMPLE
         .LINK
             https://github.com/pester/Pester/blob/master/Functions/Assertions/Should.ps1
+        .NOTES
+            Origin: PESTER
+            Credit should go to the author of PESTER
     #>
     param (
         [string]
-        $Message
+        $Message = "Houston we have a problem"
         ,
         [string]
-        $File
+        $File = "Apollo11.rocket"
         ,
         [string]
-        $Line
+        $Line = "Never stop exploring"
         ,
         [string]
-        $LineText
+        $LineText = "bebi shark"
     )
-    $exception = & $SafeCommands['New-Object'] Exception $Message
+    $exception = New-Object -TypeName "Exception" -ArgumentList $Message
     $errorID = 'PesterAssertionFailed'
     $errorCategory = [Management.Automation.ErrorCategory]::InvalidResult
     # we use ErrorRecord.TargetObject to pass structured information about the error to a reporting system.
     $targetObject = @{Message = $Message; File = $File; Line = $Line; LineText = $LineText}
-    $errorRecord = & $SafeCommands['New-Object'] Management.Automation.ErrorRecord $exception, $errorID, $errorCategory, $targetObject
+    $errorRecord = New-Object -TypeName "Management.Automation.ErrorRecord" $exception, $errorID, $errorCategory, $targetObject
     return $errorRecord
 }
